20783b4beb88645bae9b2f5a5b6b553272906797
Claudia Fox <claudiafox95@icloud.com>
2018-11-15 11:49:37 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,7 +47,7 @@ def get_average_age_for_season(data, season)
   data.fetch(season).each do |contestant|
       total_age += contestant["age"].to_f
     end
-    (total_age % number_of_contestants).round 
+    (total_age / number_of_contestants).round 
 
 end 
 
